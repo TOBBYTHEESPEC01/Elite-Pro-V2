df3ffd909feb3c0b05ca1521c6c124ebba1e5f72
TOBBYTHEESPEC01 <oluwatobilobaomotosho17@gmail.com>
2024-09-20 15:45:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,13 +6,13 @@ const chalk = require('chalk')
 global.sessionid ='put your session id here'
 
 //owmner v card
-global.ytname = "YT: ELITE-PRO-V2" //ur yt chanel name
+global.ytname = "YT: TOBBYTHEESPEC01" //ur yt chanel name
 global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
+global.botname = 'TOBBYTHEESPEC01' //ur bot name
+global.ownernumber = '2349064312474' //ur owner number
 global.ownername = 'ELITE-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
@@ -21,9 +21,9 @@ global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2"
-global.creator = "2348109263390@s.whatsapp.net"
+global.creator = "2349064312474@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["2349064312474"] // Premium User
 global.hituet = 0
 
 //bot sett
